0c10bbf5b9142454bf0511018c0b9429fbbd9725
Sylvia Chin <sylviadoce@gmail.com>
2020-11-19 10:17:19 -0800

    Fish class

---
+++ Fish.py
@@ -0,0 +1,16 @@
+#
+# Sylvia Chin
+#
+# Fish class takes care of fish movement, coordinates, "flee" mode,
+    # and death
+#
+def getNextPosition(sharkPos: list, otherFishAPos: list, otherFishBPos:
+                list) -> list:
+    return list
+
+def getDirections() -> range(0,3):
+    ds
+
+def getFleeMode() -> bool:
+    # Checking whether 
+    
