a265222c469e9fca45b773af1e6f0e26c82d0685
Benjamin A <yellowjaguar5@gmail.com>
2020-12-05 20:58:27 -0800

    Add flee mode compatability, fix animations (again)

--- SharkGUI.py
+++ SharkGUI.py
@@ -10,6 +10,7 @@ import math
 class SharkGUI:
     def __init__(self):
         self.animation_fps = 10
+        self.animation_status = [True] * 8
         self.win = GraphWin("Water World", 1200, 800, False)
         self.background = Image(Point(600, 399),
                                 "gui/fish-grid-01.png").draw(self.win)
@@ -31,33 +32,36 @@ class SharkGUI:
                                    316, 48, "Start")
         self.move_button = Button(self.win, Point(212, 664),
                                   316, 48, "Move").deactivate()
-        self.quit_button = Button(self.win, Point(212, 736), 320, 48, "")
+        self.quit_button = Button(self.win, Point(212, 736),
+                                  320, 48, "Quit")
         self.quit_button.setFill("").setOutline("")
         self.message = Text(Point(212, 568), "Enter Fish Coordinates \n"
                                              "and select Start to begin")
         self.message.draw(self.win).setTextColor("#B1E5FC")
         self.message.setSize(24)
 
-        self.rotations = [-90, 0, 0, 0]
-        self.images = [PIL.Image.open("gui/shark_low_res.png"),
-                       PIL.Image.open("gui/orange_fish_low_res.png"),
-                       PIL.Image.open("gui/yellow_fish_low_res.png"),
-                       PIL.Image.open("gui/purple_fish_low_res.png")]
-        self.fleeImages = [PIL.Image.open("gui/shark_low_res.png"),
-                           PIL.Image.open("gui/orange_fish_flee.png"),
-                           PIL.Image.open("gui/yellow_fish_flee.png"),
-                           PIL.Image.open("gui/purple_fish_flee.png")]
-        self.sprites = [Image(Point(-100, -100), "gui/shark_low_res.png")
-                        .draw(self.win),
-                        Image(Point(-100, -100),
+        self.rotations = [361] * 4
+        self.regular_images = [
+            PIL.Image.open("gui/orange_fish_low_res.png"),
+            PIL.Image.open("gui/yellow_fish_low_res.png"),
+            PIL.Image.open("gui/purple_fish_low_res.png"),
+            PIL.Image.open("gui/shark_low_res.png")]
+        self.flee_images = [PIL.Image.open("gui/orange_fish_flee.png"),
+                            PIL.Image.open("gui/yellow_fish_flee.png"),
+                            PIL.Image.open("gui/purple_fish_flee.png"),
+                            PIL.Image.open("gui/shark_low_res.png")]
+        self.images = self.regular_images[:]
+        self.sprites = [Image(Point(1300, 400),
                               "gui/orange_fish_low_res.png")
-                        .draw(self.win),
-                        Image(Point(-100, -100),
+                            .draw(self.win),
+                        Image(Point(1300, 400),
                               "gui/yellow_fish_low_res.png")
-                        .draw(self.win),
-                        Image(Point(-100, -100),
+                            .draw(self.win),
+                        Image(Point(1300, 400),
                               "gui/purple_fish_low_res.png")
-                        .draw(self.win)]
+                            .draw(self.win),
+                        Image(Point(1300, 400), "gui/shark_low_res.png")
+                            .draw(self.win)]
 
     def disableEntry(self):
         for entry in self.entries:
@@ -82,21 +86,19 @@ class SharkGUI:
     def getMovePressed(self, point) -> bool:
         return self.move_button.clicked(point)
 
-    def getQuitPressed(self, point):
+    def getQuitPressed(self, point) -> bool:
         if self.quit_button.clicked(point):
             quit("Quit Button Pressed")
+        return False
 
     def setMoveButtonLabel(self, label):
         self.move_button.setLabel(label)
 
-    def gridToCanvas(self, x, y):
-        return [x * 72 + 475, y * 72 + 75]
+    def gridToCanvas(self, xy: list):
+        return [xy[0] * 72 + 475, xy[1] * 72 + 75]
 
-    def canvasToGrid_x(self, x):
-        return (x - 440) // 72
-
-    def canvasToGrid_y(self, y):
-        return (y - 40) // 72
+    def canvasToGrid(self, xy: list):
+        return [(xy[0] - 440) // 72, (xy[1] - 40) // 72]
 
     def spriteMoveTo(self, index: int, canvas_coordinates: list):
         self.sprites[index].move(
@@ -104,14 +106,16 @@ class SharkGUI:
             canvas_coordinates[1] - self.sprites[index].getAnchor().getY())
 
     def spriteMoveOverTime(self, index: int, total_time: float,
-                           target_position: list):
-        current_position = [self.sprites[index].getAnchor().getX(),
-                            self.sprites[index].getAnchor().getY()]
+                           target_position: list, current_position=None):
+        self.animation_status[index + 4] = False
+        if not current_position:
+            current_position = [self.sprites[index].getAnchor().getX(),
+                                self.sprites[index].getAnchor().getY()]
         delta_position = [(target_position[0] - current_position[0])
                           // (total_time * self.animation_fps),
                           (target_position[1] - current_position[1])
                           // (total_time * self.animation_fps)]
-        print("target move", target_position)
+        #print("target move", index, self.canvasToGrid(current_position), self.canvasToGrid(target_position))
         self._continueSpriteMove(
             index, target_position, delta_position, 0,
             int(total_time * self.animation_fps) - 1,
@@ -127,31 +131,31 @@ class SharkGUI:
                            total_steps, delta_ms)
         else:
             self.spriteMoveTo(index, target_position)
+            self.animation_status[index + 4] = True
 
     def spriteRotate(self, index: int, abs_rotation_degrees: int):
         abs_rotation_degrees %= 360
         self.rotations[index] = abs_rotation_degrees
-        if 180 <= abs_rotation_degrees < 360:
-            self.sprites[index].img = PIL.ImageTk.PhotoImage(
-                self.images[index].rotate(
-                    abs_rotation_degrees, expand=True))
-        else:
+        if 90 <= abs_rotation_degrees <= 270:
             self.sprites[index].img = PIL.ImageTk.PhotoImage(
                 self.images[index].rotate(
                     -abs_rotation_degrees, expand=True).
-                transpose(PIL.Image.FLIP_LEFT_RIGHT))
+                    transpose(PIL.Image.FLIP_TOP_BOTTOM))
+        else:
+            self.sprites[index].img = PIL.ImageTk.PhotoImage(
+                self.images[index].rotate(
+                    abs_rotation_degrees, expand=True))
+
         self.sprites[index].undraw()
         self.sprites[index].draw(self.win)
 
     def spriteRotateOverTime(self, index: int, total_time: float,
                              target_degrees: int):
+        self.animation_status[index] = False
         target_degrees %= 360
         total_delta_degrees = target_degrees - self.rotations[index]
-        if target_degrees - self.rotations[index] >= 180:
+        if abs(target_degrees - self.rotations[index]) >= 180:
             total_delta_degrees = self.rotations[index] - target_degrees
-        # TODO: account for alt. rotation direction in delta_degrees
-        #  (makes too small jumps now)
-        print("target rotate", target_degrees, "delta", total_delta_degrees)
         self._continueSpriteRotate(
             index, total_delta_degrees
             // int(total_time * self.animation_fps),
@@ -159,14 +163,21 @@ class SharkGUI:
 
     def _continueSpriteRotate(self, index: int, delta_degrees: int,
                               target_degrees: int, delta_ms: int):
-        if not((target_degrees - abs(delta_degrees)) <
-               self.rotations[index] <
-               (target_degrees + abs(delta_degrees))):
+        if not ((target_degrees - abs(delta_degrees)) <
+                self.rotations[index] <
+                (target_degrees + abs(delta_degrees))):
             self.spriteRotate(index, self.rotations[index] + delta_degrees)
             self.win.after(delta_ms, self._continueSpriteRotate, index,
                            delta_degrees, target_degrees, delta_ms)
         else:
             self.spriteRotate(index, target_degrees)
+            self.animation_status[index] = True
+
+    def jumpToCoordinates(self, coordinates: list):
+        for i in range(len(self.sprites)):
+            self.spriteMoveTo(i, self.gridToCanvas(coordinates[i]))
+            if len(coordinates[i]) > 2:
+                self.spriteRotate(i, coordinates[i][2])
 
     def pointToList(self, point: Point):
         return [point.getX(), point.getY()]
@@ -175,55 +186,105 @@ class SharkGUI:
                        rotation_seconds: float = 1, move_seconds: int = 1):
         rotate = 0
         for i in range(len(coordinates)):
-            if (coordinates[i][:2] !=
-                    self.pointToList(self.sprites[i].getAnchor())):
-                if len(coordinates[i]) < 3:
-                    coordinates[i].append(90-int(math.degrees(math.atan2(
-                        coordinates[i][1] -
-                        self.canvasToGrid_y(
-                            self.sprites[i].getAnchor().getY()),
-                        coordinates[i][0] -
-                        self.canvasToGrid_x(
-                            self.sprites[i].getAnchor().getX())))))
-                if coordinates[i][2] != self.rotations[i]:
-                    rotate += 1
-                    self.spriteRotateOverTime(i, rotation_seconds,
-                                              coordinates[i][2])
+            current_position = self.canvasToGrid(
+                self.pointToList(self.sprites[i].getAnchor()))
+            if len(coordinates[i]) < 3:
+                coordinates[i].append(int(math.degrees(math.atan2(
+                    current_position[1] - coordinates[i][1],
+                    coordinates[i][0] - current_position[0]))))
+            coordinates, current_position = self.checkThroughMovement(
+                i, coordinates, current_position)
+            if coordinates[i][2] != self.rotations[i]:
+                rotate += 1
+                self.animation_status[i] = False
+                self.spriteRotateOverTime(i, rotation_seconds,
+                                          coordinates[i][2])
+
+                if coordinates[i][:2] != current_position:
                     self.win.after(
                         rotation_seconds * 1000, self.spriteMoveOverTime,
-                        i, move_seconds, self.gridToCanvas(
-                            coordinates[i][0], coordinates[i][1]))
-                else:
-                    self.spriteMoveOverTime(
-                        i, move_seconds, self.gridToCanvas(
-                            coordinates[i][0], coordinates[i][1]))
+                        i, move_seconds, self.gridToCanvas(coordinates[i]))
+                    self.animation_status[i + 4] = False
+            elif coordinates[i][:2] != current_position:
+                self.spriteMoveOverTime(
+                    i, move_seconds, self.gridToCanvas(coordinates[i]))
+                self.animation_status[i + 4] = False
         self.win.flush()
         if rotate:
             self.win.mainloop(rotation_seconds)
         self.win.mainloop(move_seconds)
 
+    def animationComplete(self) -> bool:
+        return all(self.animation_status)
+
+    def setFleeMode(self, in_flee_mode: list):
+        for i in range(len(in_flee_mode)):
+            if in_flee_mode[i]:
+                self.images[i] = self.flee_images[i]
+            else:
+                self.images[i] = self.regular_images[i]
+            self.spriteRotate(i, self.rotations[i])
+
+    def checkThroughMovement(self, i: int, coordinates: list,
+                             current_position: list):
+        moves = [[0, 0], [0, 0], [0, 0]]
+        if coordinates[i][0] == -1:
+            # Go through left wall
+            moves = [[-7, coordinates[i][1]],
+                     self.gridToCanvas([11, coordinates[i][1]]),
+                     self.gridToCanvas([9, coordinates[i][1]])]
+        elif coordinates[i][0] == 10:
+            # Go through right wall
+            moves = [[11, coordinates[i][1]],
+                     self.gridToCanvas([-7, coordinates[i][1]]),
+                     self.gridToCanvas([0, coordinates[i][1]])]
+        elif coordinates[i][1] == -1:
+            # Go through top wall
+            moves = [[coordinates[i][0], -2],
+                     self.gridToCanvas([coordinates[i][0], 11]),
+                     self.gridToCanvas([coordinates[i][0], 9])]
+        elif coordinates[i][1] == 10:
+            # Go through bottom wall
+            moves = [[coordinates[i][0], 11],
+                     self.gridToCanvas([coordinates[i][0], -2]),
+                     self.gridToCanvas([coordinates[i][0], 0])]
+        if moves != [[0, 0], [0, 0], [0, 0]]:
+            coordinates[i][:2] = moves[0]
+            self.win.after(1980, self.spriteMoveTo, i, moves[1])
+            self.win.after(2020, self.spriteMoveOverTime, i, 1,
+                           moves[2], moves[1])
+        return coordinates, current_position
+
 
 if __name__ == "__main__":
     testGUI = SharkGUI()
     testGUI.win.getMouse()
-    testGUI.setCoordinates([[0, 0]])
+    testGUI.jumpToCoordinates([[0, 0, 0], [0, 1, 180],
+                               [0, 2, 180], [7, 2, 0]])
     testGUI.win.getMouse()
-    testGUI.setCoordinates([[9, 0]])
+    testGUI.setCoordinates([[7, 2], [2, 7], [1, 1], [5, 4]])
     testGUI.win.getMouse()
-    testGUI.setCoordinates([[9, 9]])
+    testGUI.setFleeMode([True, False, True])
+    testGUI.setCoordinates([[7, -1], [2, 8], [1, 2], [5, 5]])
     testGUI.win.getMouse()
-    testGUI.setCoordinates([[0, 9]])
+    testGUI.setFleeMode([True, False, False])
+    testGUI.setCoordinates([[0, 6]])
     testGUI.win.getMouse()
-
-    testGUI.setCoordinates([[0, 0, 0]])
-    testGUI.win.getMouse()
-    testGUI.setCoordinates([[0, 0, 359]], 5, 1)
-    testGUI.win.getMouse()
-    testGUI.setCoordinates([[0, 9]])
+    testGUI.setCoordinates([[-1, 6]])
     testGUI.win.getMouse()
     testGUI.setCoordinates([[9, 9]])
     testGUI.win.getMouse()
-    testGUI.setCoordinates([[0, 1]])
+    testGUI.setCoordinates([[10, 9]])
+    testGUI.win.getMouse()
+    testGUI.setCoordinates([[9, 3]])
+    testGUI.win.getMouse()
+    testGUI.setCoordinates([[10, 3]])
+    testGUI.win.getMouse()
+    testGUI.setCoordinates([[0, 7]])
+    testGUI.win.getMouse()
+    testGUI.setCoordinates([[-1, 7]])
+    testGUI.win.getMouse()
+    testGUI.setCoordinates([[9, -1]])
 
     testGUI.win.getMouse()
     testGUI.disableEntry()
