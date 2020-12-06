8f303d5c5dc80e6879a2d39ea1a775f0ed3b24e7
Benjamin A <yellowjaguar5@gmail.com>
2020-12-05 21:42:54 -0800

    Shark: continue following previous target if equidistance from another fish

--- Shark.py
+++ Shark.py
@@ -10,29 +10,35 @@ class Shark:
     def __init__(self):
         self.position = [7, 2, 0]
         self.max_distance = 2.49  # Max direct distance
+        self.following_fish = -1
 
     def getNextPosition(self, fish_pos: list) -> list:
         distances = []
         for pos in fish_pos:
             distances.append(math.dist(pos[:2], self.position[:2]))
-        shuffle(distances)
-        fish_distance = min(distances)
-        closest_fish = fish_pos[distances.index(fish_distance)]
-        delta_position = [
-            round(min(self.max_distance, fish_distance) * (
-                (closest_fish[0] - self.position[0]) / fish_distance)),
-            round(min(self.max_distance, fish_distance) * (
-                (closest_fish[1] - self.position[1]) / fish_distance))]
-        self.position = [self.position[0] + delta_position[0],
-                         self.position[1] + delta_position[1],
-                         math.degrees(math.atan2(
-                             closest_fish[1] - self.position[1],
-                             closest_fish[0] - self.position[0]))]
+
+        if ((sorted(distances)[0] == sorted(distances)[1] and
+             0 <= self.following_fish <= 1) or
+            (sorted(distances)[0] == sorted(distances)[2])
+                and self.following_fish > 0):
+            closest_fish = fish_pos[self.following_fish]
+        else:
+            shuffle(distances)
+            self.following_fish = distances.index(min(distances))
+            closest_fish = fish_pos[self.following_fish]
+        fish_distance = math.dist(closest_fish[:2], self.position[:2])
+
+        self.position = [self.position[0] + round(
+                    min(self.max_distance, fish_distance) * (
+                    (closest_fish[0] - self.position[0]) / fish_distance)),
+                         self.position[1] + round(
+                    min(self.max_distance, fish_distance) * (
+                    (closest_fish[1] - self.position[1]) / fish_distance))]
         return self.position
 
 
 if __name__ == "__main__":
     shark = Shark()
     print(shark.position)
-    print(shark.getNextPosition([[7, 1]]))
-    print(shark.getNextPosition([[1, 1]]))
+    print(shark.getNextPosition([[1, 1], [1, 2], [1, 3]]))
+    print(shark.getNextPosition([[2, 1], [2, 2], [2, 3]]))
