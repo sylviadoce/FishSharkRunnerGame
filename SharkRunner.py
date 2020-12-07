e22db9abc97f3a4392b609f1e09e28746381ea9f
Sylvia Chin <sylviadoce@gmail.com>
2020-12-07 09:26:05 -0800

    Update SharkRunner.py

--- SharkRunner.py
+++ SharkRunner.py
@@ -10,9 +10,17 @@ from SharkGUI import SharkGUI
 
 def __init__(self):
 
+    shark_GUI = SharkGUI()
+
+    
+
     # Create 3 fish
+    fish_A = Fish(fish_A, [])
+    fish_B = Fish(fish_B, [])
+    fish_C = 
 
     # Create 1 shark
+    shark = 
 
     while #blahh condition:
     
